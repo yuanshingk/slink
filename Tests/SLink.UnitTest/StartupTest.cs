eac1e28292096c75fd9dd33f07058e42be2baddb
kong yuan shing <yuanshing.kong@gmail.com>
2019-11-11 21:20:39 +0800

    Add Unit test for Dependency Injection

---
+++ Tests/SLink.UnitTest/StartupTest.cs
@@ -0,0 +1,25 @@
+﻿using HashidsNet;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SLink.Providers;
+using SLink.Repositories;
+using SLink.Services;
+using Xunit;
+
+namespace SLink.UnitTest
+{
+    public class StartupTest
+    {
+        [Fact]
+        public void Startup_ServiceDependenciesAreRegistered()
+        {
+            var webHost = Microsoft.AspNetCore.WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
+           
+            Assert.NotNull(webHost);
+            Assert.NotNull(webHost.Services.GetRequiredService<IShortLinkService>());
+            Assert.NotNull(webHost.Services.GetRequiredService<IDataProvider>());
+            Assert.NotNull(webHost.Services.GetRequiredService<IRepository>());
+            Assert.NotNull(webHost.Services.GetRequiredService<IHashids>());
+        }
+    }
+}
