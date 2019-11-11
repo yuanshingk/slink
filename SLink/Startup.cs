375d5a902f144e2e1be8bc419170050c11981fc3
kong yuan shing <yuanshing.kong@gmail.com>
2019-11-11 21:20:18 +0800

    Remove Error page

--- SLink/Startup.cs
+++ SLink/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.OpenApi.Models;
 using SLink.Providers;
 using SLink.Repositories;
 using SLink.Services;
-using System;
 
 namespace SLink
 {
@@ -52,8 +51,6 @@ namespace SLink
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
